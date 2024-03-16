3bb18fdb70c44a2a863b9854c3f82c02630903f5
Nevil163 <nevilpatel35@gmail.com>
2024-03-16 12:45:47 -0700

    updated title

--- go-canvas/src/App.tsx
+++ go-canvas/src/App.tsx
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Hello X</p>
-          Go-Canvas
-        </a>
+          Go-Canvas 2024
+        
       </header>
     </div>
   );
