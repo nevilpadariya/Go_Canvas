0796c796e43333ba6f3d3c4c6402614b4bca1f57
robinhood074 <yagniksatani123@gmail.com>
2024-04-12 11:35:10 +0530

    fixed login navigation issue

--- go-canvas/src/pages/login.tsx
+++ go-canvas/src/pages/login.tsx
@@ -15,7 +15,7 @@ import { loginlogo } from "../assets/images";
 import { checkBoxChecked } from "../assets/images";
 import { checkBox } from "../assets/images";
 import Header from "../components/header";
-
+import { useNavigate } from 'react-router-dom';
 function CheckboxDefault() {
   return <img src={checkBox} alt="checkbox" />;
 }
@@ -32,13 +32,15 @@ const Login: React.FC = () => {
   const [user, setUser] = useState<User>({ username: "", password: "" });
   const [error, setError] = useState<string>("");
   const [token, setToken] = useState<string | null>(null);
-
+  // ? - for navigation to another page
+  const navigate = useNavigate();
   useEffect(() => {
     // Check if token exists in local storage (assuming it's saved securely after login)
     const savedToken = localStorage.getItem("token");
     if (savedToken) {
       setToken(savedToken);
       // Optionally, you can redirect the user to a dashboard or home page
+      navigate('dashboard');
     }
   }, []);
 
@@ -46,21 +48,28 @@ const Login: React.FC = () => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
+  const payload = {
+    username : user.username,
+    password: user.password
+  }
   const handleLogin = async () => {
+    // TODO : replace api url 'your-api-endpoint/login' with backend provided url...
     try {
-      const response = await fetch("your-api-endpoint/login", {
+      const response = await fetch("https://dummyjson.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(user),
+        body: JSON.stringify(payload),
       });
-
+      console.log('api response===>>',response);
+      
       if (response.ok) {
         const data = await response.json();
         const token = data.token; // Assuming your API returns a token upon successful login
         localStorage.setItem("token", token); // Save token securely in local storage
         setToken(token);
+        navigate('dashboard')
       } else {
         const data = await response.json();
         setError(data.message);
@@ -109,7 +118,6 @@ const Login: React.FC = () => {
         <Card>
           <CardContent>
             <Box mt={5}>
-              {!token ? (
                 <Box>
                   {error && (
                     <Typography variant="body1" color="error">
@@ -174,24 +182,6 @@ const Login: React.FC = () => {
                     </div>
                   </form>
                 </Box>
-              ) : (
-                <Box>
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={handleLogout}
-                    style={{ marginRight: "10px" }}
-                  >
-                    Logout
-                  </Button>
-                  <Button
-                    variant="contained"
-                    onClick={sendAuthenticatedRequest}
-                  >
-                    Send Authenticated Request
-                  </Button>
-                </Box>
-              )}
             </Box>
           </CardContent>
         </Card>
