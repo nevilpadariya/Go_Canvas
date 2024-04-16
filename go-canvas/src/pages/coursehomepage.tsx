c781ba728b509bff7a1f8ed515e854bc2959a94c
Nagaraj-Gireppa-Kanni <nagarajgireppa.kanni@sjsu.edu>
2024-04-16 14:18:17 -0700

    new coursehomepage

---
+++ go-canvas/src/pages/coursehomepage.tsx
@@ -0,0 +1,7 @@
+import { Button, TextField } from "@mui/material";
+import React from "react";
+import Header from "../components/header";
+import Sidebar from "../components/sidebar";
+import { Helmet } from "react-helmet";
+
+export default function coursehomepage() {}
